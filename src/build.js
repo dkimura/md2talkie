ad3a74504a621181c91c68629a0efbff2e10cbab
Daichi Kimura <dkimura@bdd.xyz>
2017-03-29 01:24:47 +0900

    feat: add error handler

--- src/build.js
+++ src/build.js
@@ -5,13 +5,34 @@ import type { Slides } from '../templates/types/Slides';
 
 export default (props: Slides) => {
   if (process.env.NODE_ENV === 'development') {
-    return webpack(webpackConfig(props)).watch({}, (err) => {
-      if (err) throw Error(err);
+    return webpack(webpackConfig(props)).watch({}, (err, stats) => {
+      if (err) {
+        console.log('Failed to compile.');
+        console.log(err);
+        process.exit(1);
+      }
+
+      if (stats.compilation.errors.length) {
+        console.log('Failed to compile.');
+        console.log(stats.compilation.errors);
+        process.exit(1);
+      }
     });
   }
 
-  return webpack(webpackConfig(props)).run((err) => {
-    if (err) throw Error(err);
-    return console.log('build success.');
+  return webpack(webpackConfig(props)).run((err, stats) => {
+    if (err) {
+      console.log('Failed to compile.');
+      console.log(err);
+      process.exit(1);
+    }
+
+    if (stats.compilation.errors.length) {
+      console.log('Failed to compile.');
+      console.log(stats.compilation.errors);
+      process.exit(1);
+    }
+
+    console.log('Build Success.');
   });
 };
